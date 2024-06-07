95ac8dd096a1c04d42f0db2879e828b6ed2f6967
Naren-2001-k <k.naren2001@gmail.com>
2024-06-07 10:15:01 +0530

    Form

---
+++ Form.java
@@ -0,0 +1,23 @@
+public class Form{
+
+public static void main(String[] args){
+
+String myName="Sangeetha";
+char myInitial='R';
+byte myAge=29;
+int myIncome=20000;
+long myBank=20000110;
+float myInterest=25.204646f;
+boolean isLearning = true;
+
+System.out.println("My Name:" +myName);
+System.out.println("My initial:" +myInitial);
+System.out.println("My age:"+myAge);
+System.out.println("My income:"+myIncome);
+System.out.println("My bank balance:"+myBank);
+System.out.println("My interest:"+myInterest);
+System.out.println("I am learning JAVA:"+isLearning);
+
+
+
+}}
